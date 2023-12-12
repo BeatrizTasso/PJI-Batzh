0bc133e6c518db80c30f52035bfe1389cac61935
Gabriel <gabrielmgruppogg@gmail.com>
2023-12-12 09:07:43 -0300

    tst

--- batzh/client/src/components/Header/Header.js
+++ batzh/client/src/components/Header/Header.js
@@ -8,7 +8,7 @@ import { IoLogOut } from 'react-icons/io5'
 import './Header.css'
 function Header() {
     const { userData, setUserData } = useContext(Context)
-    const page = window.open('./pages/register')
+    //const page = window.open('./pages/register')
 
  //    page.addEventListener('DOMContentLoaded', () => {
  //       const EmpCli = page.document.getElementById('status')
